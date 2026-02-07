3100490c417606c23ca0d3d257f4a2f01d1e8c99
Dewa Ngurah <65481663+dewangr@users.noreply.github.com>
2026-02-08 03:12:59 +0800

    move funtions  to new file

---
+++ src/process/addItem.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { API_URL } from "../types/consts.js";
+import { getRsvpData } from "./getRsvpData.js";
+
+export async function addItem(namaTamu, ucapan, akanHadir) {
+  try {
+    const response = await axios.post(
+      `${API_URL}/api/rsvps`,
+      {
+        nama_tamu: namaTamu.value,
+        ucapan: ucapan.value,
+        kehadiran: akanHadir ? akanHadir.value : null,
+      }
+    );
+    console.log(response);
+  } catch (err) {
+    console.error("There has been a problem with your fetch operation: ", err);
+  }
+
+  await getRsvpData(); // Refresh data after adding new item
+}
---
+++ src/process/getRsvpData.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { ref } from "vue";
+import { API_URL } from "../types/consts.js";
+
+export async function getRsvpData() {
+  const rspvData = ref([]);
+
+  try {
+    const response = await axios.get(`${API_URL}/api/rsvps`);
+    rspvData.value = response.data.data;
+    return rspvData;
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
+}
