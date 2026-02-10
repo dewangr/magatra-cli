0868b8a9ff03162ff1ced215f6af94adfa9e3361
Dewa Ngurah <65481663+dewangr@users.noreply.github.com>
2026-02-11 02:30:51 +0800

    fix interface for wish' card

--- src/components/bubbles/kehadiranText.vue
+++ src/components/bubbles/kehadiranText.vue
@@ -36,18 +36,18 @@ onUnmounted(() => {
 });
 </script>
 <template>
-  <div class="textUcapan p-3 rounded-sm mb-4 mx-2 opacity-70">
+  <div class="textUcapan text-off-white bg-alt-dark p-3 rounded-sm mb-4 mx-2 opacity-70">
     <div class="namaDanKonfirmasi flex flex-row text-[16px] items-center justify-between">
-      <p class="namaTamuReservasi text-alt-dark tracking-wider">
+      <p class="namaTamuReservasi text-off-white tracking-wider text-sm">
         <b>{{ namaTamu }} &nbsp;</b>
       </p>
-     <span :hidden="!props.konfirmasiKehadiran" class="text-off-white bg-alt-dark w-max px-2 py-1 rounded-xl text-[11px] flex items-center"> <i
+     <span :hidden="!props.konfirmasiKehadiran" class="bg-off-white text-alt-dark w-max px-2 py-1 rounded-xl text-[11px] flex items-center"> <i
                    aria-hidden="true"></i> &nbsp; {{
                         hadirString
                     }}</span>
     </div>
-    <p class="text-[16px] text-alt-dark mb-1">{{ ucapan }}</p>
-    <p class="text-[10px] text-alt-dark/50 italic">{{ waktuKomentar }}</p>
+    <p class="text-sm text-off-white mb-1">{{ ucapan }}</p>
+    <p class="text-[10px] text-off-white/50 italic">{{ waktuKomentar }}</p>
   </div>
 </template>
 <style scoped>
