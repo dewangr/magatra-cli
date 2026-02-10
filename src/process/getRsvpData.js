62623c2091c6ef342dc552e33f879fba5b5d24af
Dewa Ngurah <65481663+dewangr@users.noreply.github.com>
2026-02-11 01:20:29 +0800

    delete .env and move api url to consts.js

--- src/process/getRsvpData.js
+++ src/process/getRsvpData.js
@@ -4,10 +4,15 @@ import { API_URL } from "../types/consts.js";
 
 export async function getRsvpData() {
   const rspvData = ref([]);
+  const endpoint = `${API_URL}/rsvps`;
+  console.log("Fetching RSVP data from:", endpoint);
 
   try {
-    const response = await axios.get(`${API_URL}/rsvps`);
+    const response = await axios.get(endpoint);
+    console.log("RSVP data fetched:", response.data);
     rspvData.value = response.data.data;
+    console.log("RSVP data assigned to rspvData:", rspvData.value);
+
     return rspvData;
   } catch (error) {
     console.error("There has been a problem with your fetch operation:", error);
