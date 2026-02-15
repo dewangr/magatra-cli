7a65339b9997a966bfed52c7127ea2894f59b0d1
Dewa Ngurah <65481663+dewangr@users.noreply.github.com>
2026-02-15 17:53:40 +0800

    install prerender and update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin-next");
+
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -6,6 +9,10 @@ module.exports = defineConfig({
     plugins: [
       new (require("webpack").DefinePlugin)({
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "false",
+      }),
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, "dist"),
+        routes: ["/", "/dekdwi-manik/"],
       }),
     ],
   },
