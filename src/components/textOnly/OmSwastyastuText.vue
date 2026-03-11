74d7f5c920a0905106dbe468d0705a93404e2027
Dewa Ngurah <65481663+dewangr@users.noreply.github.com>
2026-03-12 00:59:09 +0800

    make osa component to be more flexible

--- src/components/textOnly/OmSwastyastuText.vue
+++ src/components/textOnly/OmSwastyastuText.vue
@@ -1,6 +1,17 @@
 <script setup>
 import "@/assets/main.css";
+import { defineProps } from "vue";
 
+const props = defineProps ({
+    textTittle: {
+        type: String,
+        default: "Om Swastyastu"
+    },
+    textContent: {
+        type: String,
+        default: "Atas Asung Kerta Wara Nugraha Ida Sang Hyang Widhi Wasa, kami bermaksud mengundang Bapak/Ibu/Saudara/i untuk hadir pada upacara pawiwahan anak kami."
+    }
+})
 </script>
 <template>
     <div class="relative w-full flex flex-col">
@@ -8,13 +19,11 @@ import "@/assets/main.css";
             <div
                 class="main-content relative z-20 flex flex-col justify-center text-center sm:w-[60%] md:w-full h-[55%] mx-7">
                 <div>
-                    <p class="font-parisienne text-putih-keabuan text-4xl mb-3 animated">
-                        Om Swastyastu
+                    <p class="font-parisienne text-off-white text-4xl mb-3 animated">
+                        {{props.textTittle}}
                     </p>
                     <p class="text-sm text-putih-keabuan animated">
-                        Atas Asung Kerta Wara Nugraha Ida Sang Hyang Widhi Wasa, kami
-                        bermaksud mengundang Bapak/Ibu/Saudara/i untuk hadir pada upacara
-                        telubulanan anak kami.
+                       {{props.textContent}}
                     </p>
                 </div>
             </div>
