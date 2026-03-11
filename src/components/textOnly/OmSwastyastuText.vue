b58d015e5f630c53a19849177bcc3e5d5e043809
Dewa Ngurah <65481663+dewangr@users.noreply.github.com>
2026-03-11 18:10:50 +0800

    add animation

--- src/components/textOnly/OmSwastyastuText.vue
+++ src/components/textOnly/OmSwastyastuText.vue
@@ -8,10 +8,10 @@ import "@/assets/main.css";
             <div
                 class="main-content relative z-20 flex flex-col justify-center text-center sm:w-[60%] md:w-full h-[55%] mx-7">
                 <div>
-                    <p class="font-parisienne text-light-yellow text-4xl mb-3 ">
+                    <p class="font-parisienne text-putih-keabuan text-4xl mb-3 animated">
                         Om Swastyastu
                     </p>
-                    <p class="text-sm text-putih-keabuan">
+                    <p class="text-sm text-putih-keabuan animated">
                         Atas Asung Kerta Wara Nugraha Ida Sang Hyang Widhi Wasa, kami
                         bermaksud mengundang Bapak/Ibu/Saudara/i untuk hadir pada upacara
                         telubulanan anak kami.
